bb01da0841f3bad672329948ccc5726a12596690
Neil Barnedo <michaelsbarnedo@gmail.com>
2024-04-15 16:31:39 +0800

    Added starting screen

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+// import OverviewScreenNavigation from './src/components/navigations/OverviewScreenNavigation';
+import Overview from './src/components/overview/Overview';
+
+// import AppLoading from 'expo-app-loading';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from 'expo-font';
+import { RootSiblingParent } from 'react-native-root-siblings';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  let [fontsLoaded] = useFonts({
+    'Nunito-Bold': require('./src/fonts/Nunito-Bold.ttf'),
+    'Poppins-Regular': require('./src/fonts/Poppins-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    SplashScreen.hideAsync();
+    return null;
+  } 
+  
+  return <Overview />;
+
+}
